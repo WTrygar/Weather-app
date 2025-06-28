41c8c0254b14db76e91d102977a979369a6edc6b
WTrygar <chandelier.wt@gmail.com>
2025-06-28 14:27:53 +0200

    fix: finally working

---
+++ src/components/SiteNavigation.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="sticky top-0 bg-blue-400 shadow-lg">
+    <nav
+      class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6"
+    >
+      <RouterLink :to="{ name: 'home' }">
+        <div class="flex items-center gap-3 flex-1">
+          <i class="fa-solid fa-sun text-2xl"></i>
+          <p class="text-2xl">The local Weather</p>
+        </div>
+      </RouterLink>
+
+      <div class="flex gap-3 flex-1 justify-end">
+        <i
+          class="fa-solid fa-circle-info text-xl hover:text-blue-200 duration-150 cursor-pointer"
+        >
+        </i>
+        <i
+          class="fa-solid fa-plus text-xl hover:text-blue-200 duration-150 cursor-pointer"
+        ></i>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
