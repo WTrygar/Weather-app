d358ebaf1fe80afbeb3330154a86b085c93f8fbe
WTrygar <chandelier.wt@gmail.com>
2025-07-02 19:16:43 +0200

    feat: working information button

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <div class="flex flex-col min-h-screen font-Roboto bg-midnight">
-    <h2>hello</h2>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
     <SiteNavigation />
     <RouterView />
   </div>
